59798121279317993ce5613df991bc6d8c4d4228
Aaron <aaronrobinett@gmail.com>
2021-10-21 21:13:44 -0700

    added votes add and get

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,10 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    // Your MySQL username,
-    user: 'root',
-    // Your MySQL password
-    password: 'SystemofaDown6969',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: 'election'
 });
 
